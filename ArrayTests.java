868bb425d509c1621b0c79a5f2627eab6f0003db
luckicode <74932717+luckicode@users.noreply.github.com>
2024-02-01 13:43:10 -0800

    werf
    fgrg

--- ArrayTests.java
+++ ArrayTests.java
@@ -7,12 +7,45 @@ public class ArrayTests {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
+
+    int[] oddArray2 = { 1, 2, 3 };
+    ArrayExamples.reverseInPlace(oddArray2);
+    assertArrayEquals(new int[]{ 3, 2, 1 }, oddArray2);
+
+    int[] evenArray2 = { 1, 2, 3, 4 };
+    ArrayExamples.reverseInPlace(evenArray2);
+    assertArrayEquals(new int[]{ 4, 3, 2, 1 }, evenArray2);
+
+    int[] evenArrayNeg = { 1, 2, -3, 4 };
+    ArrayExamples.reverseInPlace(evenArrayNeg);
+    assertArrayEquals(new int[]{ 4, -3, 2, 1 }, evenArrayNeg);
+
+
 	}
 
 
   @Test
   public void testReversed() {
     int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    assertArrayEquals(new int[]{}, ArrayExamples.reversed(input1));
+
+    int[] oddArray = { 1, 2, 3};
+    assertArrayEquals(new int[]{ 3, 2, 1 }, ArrayExamples.reversed(oddArray));
+
+    int[] evenArray = { 1, 2, 3, 4,};
+    assertArrayEquals(new int[]{ 4, 3, 2, 1 }, ArrayExamples.reversed(evenArray));
+
+    int[] evenArrayNeg = { 1, 2, -3, 4,};
+    assertArrayEquals(new int[]{ 4, -3, 2, 1}, ArrayExamples.reversed(evenArrayNeg));
+
+    }
+
+  
+
+  @Test
+  public void testAverageWithoutLowest(){
+    double[] input1 = {1.0, 1.0, 2.0, 3.0, 4.0};
+    assertEquals(3.0, ArrayExamples.averageWithoutLowest(input1),0);
   }
 }
+
